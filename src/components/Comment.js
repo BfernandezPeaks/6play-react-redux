a808550b644a5e611d464fc1281ea30eaf64eab9
BfernandezPeaks <bfernandez@peaks.fr>
2019-02-25 15:35:12 +0100

    :art: comment style :sparkles: comment fonctionnality

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import './Comment.css';
 
 class Comment extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    this.props.delete(this.props.index);
+  }
+
   render() {
     return (
-      <pre>{this.props.value}</pre>
+      <div className="comment">
+        <pre>{this.props.value.text}</pre>
+        <div className="delete" onClick={this.handleClick}>x</div>
+        <div className="date">{this.props.value.date}</div>
+      </div>
     );
   }
 }
